890b67516129e9309a099aba19f3246467f63b85
mdabarik <mdabarik19@gmail.com>
2024-11-22 18:53:12 +0600

    first and

--- dist/server.js
+++ dist/server.js
@@ -13,12 +13,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const { Schema } = mongoose_1.default;
+const config_1 = __importDefault(require("./app/config"));
+const app_1 = __importDefault(require("./app"));
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield mongoose_1.default.connect(process.env.DATABASE_URL);
-        app.listen(port, () => {
-            console.log(`Example app listening on port ${port}`);
-        });
+        try {
+            yield mongoose_1.default.connect(config_1.default.database_url);
+            app_1.default.listen(config_1.default.port, () => {
+                console.log(`Example app listening on port ${config_1.default.port}`);
+            });
+        }
+        catch (error) {
+            console.log(error);
+        }
     });
 }
+main();
