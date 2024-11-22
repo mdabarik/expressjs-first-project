ae78da95b944a1abe285bd3b08766aff74fe6ef5
mdabarik <mdabarik19@gmail.com>
2024-11-22 23:34:41 +0600

    solved few issues

--- test.js
+++ test.js
@@ -1,25 +1,26 @@
 {
-    var calculateShapeArea = function (shape) {
-        var area = 0;
-        if ("radius" in shape) { // in operator
-            area = Math.PI * shape.radius * shape.radius;
-        }
-        else if ("height" in shape && "width" in shape) { // in operator
-            area = shape.height * shape.width;
-        }
-        return parseFloat(area.toFixed(2));
-    };
-    var shape1 = {
-        name: "circle",
-        radius: 5
-    };
-    var shape2 = {
-        name: "rectangle",
-        width: 4,
-        height: 6
-    };
-    var circleArea = calculateShapeArea(shape1);
-    var rectangleArea = calculateShapeArea(shape2);
-    console.log(circleArea, rectangleArea);
-    // 
+  var calculateShapeArea = function (shape) {
+    var area = 0
+    if ('radius' in shape) {
+      // in operator
+      area = Math.PI * shape.radius * shape.radius
+    } else if ('height' in shape && 'width' in shape) {
+      // in operator
+      area = shape.height * shape.width
+    }
+    return parseFloat(area.toFixed(2))
+  }
+  var shape1 = {
+    name: 'circle',
+    radius: 5,
+  }
+  var shape2 = {
+    name: 'rectangle',
+    width: 4,
+    height: 6,
+  }
+  var circleArea = calculateShapeArea(shape1)
+  var rectangleArea = calculateShapeArea(shape2)
+  console.log(circleArea, rectangleArea)
+  //
 }
