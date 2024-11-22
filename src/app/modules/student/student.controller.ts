27b7ae2b6bfde9fa49590e3a06e1e2f77e2dbcfd
mdabarik <mdabarik19@gmail.com>
2024-11-22 23:32:18 +0600

    added single student

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -3,7 +3,7 @@ import { StudentServices } from './student.service'
 
 const createStudent = async (req: Request, res: Response) => {
   try {
-    const {student: studentData} = req.body
+    const { student: studentData } = req.body
     // will call service function to send this data
     const result = await StudentServices.createStudentIntoDB(studentData)
     // send response
@@ -17,6 +17,35 @@ const createStudent = async (req: Request, res: Response) => {
   }
 }
 
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB()
+    res.status(200).json({
+      success: true,
+      message: 'students are retrieved successfully',
+      data: result,
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params
+    const result = await StudentServices.getSingleStudentFromDB(studentId)
+    res.status(200).json({
+      success: true,
+      message: 'students are retrieved successfully',
+      data: result,
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export const StudentController = {
   createStudent,
+  getAllStudents,
+  getSingleStudent,
 }
--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -5,4 +5,8 @@ const router = express.Router()
 // will call ontroller functions
 router.post('/create-student', StudentController.createStudent)
 
+router.get('/', StudentController.getAllStudents)
+
+router.get('/:studentId', StudentController.getSingleStudent)
+
 export const StduentRoutes = router
